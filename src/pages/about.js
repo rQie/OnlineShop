9c1a4e8f1fbe4e2109a4ef45428f8aac9980a6db
Rifqie <rifqieakma@gmail.com>
2020-11-02 17:00:54 +0800

    Add About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
 const About = () => (
   <Layout>
-    <div         style={{
-          margin: `0 auto `,
-         marginTop: `1.45rem`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}>
-    <h1>Hi from the About page</h1>
-    <p>Welcome to page About</p>
-    <Link to="/">Go back to the homepage</Link>
+    <section>
+    <div className="row featurette">
+      <div className="col-md-7">
+        <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+        <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div className="col-md-5">
+        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      </div>
     </div>
+
+    <hr class="featurette-divider"/>
+
+    <div className="row featurette">
+      <div className="col-md-7 order-md-2">
+        <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
+        <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div className="col-md-5 order-md-1">
+        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+    </section>
+
   </Layout>
 )
 
